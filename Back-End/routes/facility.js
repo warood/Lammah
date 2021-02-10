d34db61b373e71eb4960d1d45ddb22eec34c2454
talamajed <tala.majed.g@gmail.com>
2021-02-10 14:39:46 +0300

    appointment , facility , and user models, facility and user routes

---
+++ Back-End/routes/facility.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const Facility = require('../models/facility')
+
+// display all facilities
+router.get("/facilities", (req, res) => {
+    Facility.find().sort({updatedAt: -1}).exec()
+       .then((facilities) => {
+          res.json({ msg: facilities });
+       })
+       .catch((err) => res.json({ msg: err }));
+  });
+
+
+// add a new Facility
+router.post("/new-facility", (req, res) => {
+    console.log(req.body);
+    const { name ,images , location, description , city , price ,type , appointment} = req.body;
+
+    Facility.create({ name : name ,images : images , location :location ,description : description
+    , city : city , price : price , type : type , appointment: appointment}, (err, newFacility) => {
+
+        console.log("new Facility: ", newFacility);
+        res.json({msg: "add Facility", newFacility})
+       
+    });
+});
+
+
+// display one facility
+router.get("/facilities/:id", (res,req)=>{
+   let id = req.params.id
+   console.log(id)
+   Facility.findById(id)
+    .then(facility=>{
+       console.log(facility)
+       res.json({msg:facility})
+    })
+})
+
+
+
+module.exports = router
---
+++ Back-End/routes/user.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+//for authentication
+router.get("/", (req, res) => {
+    User.find()
+    .then((users) => {
+      res.json({ msg: users });
+    })
+    .catch((err) => res.json({ msg: err }));
+});
+
+
+router.get("/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json({ msg: users });
+    })
+    .catch((err) => res.json({ msg: err }));
+});
+
+// sign up
+router.post("/register", (req, res) => {
+   const newUser = {
+     email: req.body.email,
+     password: req.body.password,
+     name: req.body.name,
+     phone: req.body.name,
+   };
+
+   newUser.email = newUser.email.toLowerCase();
+   User.findOne({ email: newUser.email })
+     .then((user) => {
+      // if the email exist
+     if (user) {
+        res.json({
+          msg: "This email already exist !",
+        });
+      }
+      // if the email doesn't exist
+      else {
+        var salt = bcrypt.genSaltSync(10);
+        newUser.password = bcrypt.hashSync(req.body.password, salt);
+        newUser.email = newUser.email.toLowerCase();
+        User.create(newUser).then((user) => {
+        res.json({ msg: "User has been registerd", user: user });
+        });
+      }
+    })
+    .catch((err) => res.json({ msg: err }));
+});
+
+
+//login
+router.post("/login", async (req, res) => {
+   let { email, password } = req.body;
+ 
+   email = email.toLowerCase();
+   const user = await User.findOne({ email: email }); 
+
+  // if the email doesn't exist
+  if (!user) {
+    res.json({ msg: "This email does not exist" });
+  }
+  //  if the email exist
+  else {
+    // if password is correct
+    if (bcrypt.compareSync(password, user.password)) {
+      user.password = undefined;
+      let payload = { user };
+      let token = jwt.sign(payload, process.env.SECRET_KEY, {
+        expiresIn:   60 * 60 * 60 * 1000 * 60 * 60,
+      }); // to the user info
+      res.json({ msg: "User login ", token });
+    }
+    // if password is not correct
+    else {
+      res.json({ msg: "Password is not correct" });
+    }
+  }
+});
+
+
+router.get("/:token", (req, res) => {
+  let token = req.params.token;
+  jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
+    if (err) return res.json({ msg: err });
+      let user = decode;
+      res.json({ msg: "User decoded", user });
+  });
+}); 
+
+
+//forgot password
+router.post('/forgot' , (req,res)=>{
+
+  User.findOne({email: req.body.email}, function (err, foundUser) {
+
+    password = req.body.password;
+    console.log(foundUser);
+    bcrypt.genSalt((err, salt) => {
+      // changes every time
+          console.log("bcrypt salt:", salt);
+          bcrypt.hash(password, salt, (err, passwordHash) => {
+          console.log("password:", password);
+          console.log("passwordHash:", passwordHash);
+          User.updateOne({ email :req.body.email },{password: passwordHash }, (err, newUser) => {
+            
+          });
+      });
+  });
+    if (!foundUser) {
+      console.log('No user with email ' + email);
+    }  
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+module.exports = router;
