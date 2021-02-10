444749045bfce6e060c0e973660c4a677fca4c5a
warood <warood21@gmail.com>
2021-02-10 18:00:53 +0300

    signup login newfacility jsx css

--- Front-End/lammah/src/App.js
+++ Front-End/lammah/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react" ;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SignUp from './pages/SignUp'
+import NewFacility from './pages/NewFacility'
+import Login from './pages/Login'
+import jwt_decode from "jwt-decode";
+
 
 function App() {
+
+  const [dataLoading, setDataloading] = useState(false)
+  const [auth, setAuth] = useState({ currentUser: null, isLoggedIn: false });
+
+  const userLogin = () => {
+    if (localStorage.jwtToken) {
+      const jwtToken = localStorage.jwtToken;
+      const currentUser = jwt_decode(jwtToken, "SECRET").user;
+      setAuth({ currentUser, isLoggedIn: true });
+    } else {
+      setAuth({ currentUser: null, isLoggedIn: false });
+    }
+
+    setDataloading(true)
+  };
+  console.log("The current User is: ", auth.currentUser, "data loading", dataLoading);
+
+  useEffect(userLogin, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+        <Route path="/signup">
+
+            <SignUp  />
+        </Route>
+        <Route path="/new">
+
+            <NewFacility  />
+        </Route>
+
+        <Route loginCallback={userLogin} auth={auth} path="/login">
+
+            <Login  />
+        </Route>
+    </Router>
   );
 }
 
