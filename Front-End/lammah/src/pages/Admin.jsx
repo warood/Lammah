1d7efc9974f62841cffc46abf8816350bbfb6d95
talamajed <tala.majed.g@gmail.com>
2021-02-13 15:12:08 +0300

    admin page

---
+++ Front-End/lammah/src/pages/Admin.jsx
@@ -0,0 +1,164 @@
+
+import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import { Link } from 'react-router-dom';
+
+export default function Admin(props) {
+
+    
+    const [facilities, setFacilities] = useState([])
+    const [users, setUsers] = useState([])
+    const [changeAfterDelete, setChangeAfterDelete] = useState(false)
+
+    useEffect(() => {
+        
+
+        /*     *****    GET ALL Facilities    *****      */
+        axios.get('http://localhost:5000/api/facility/facilities')
+            .then(res => {
+                setFacilities(res.data.facilities) 
+            })
+
+
+        /*     *****    GET ALL USERS    *****      */
+        axios.get(`http://localhost:5000/api/admin/users`)
+            .then(res => {
+              
+                console.log("api admin users "+res.data)
+                setUsers(res.data.msg)
+            })
+
+    }, [changeAfterDelete])
+
+    const changeUserPermission = (e, user) =>{
+       
+        const isAdmin = user.isAdmin
+        console.log(isAdmin)
+        if(e==false&&isAdmin==true || e==true&&isAdmin==false){
+            axios.put(`http://localhost:5000/api/admin/${user._id}`, {isAdmin: isAdmin})
+            .then(res=>{
+                console.log(res)
+            })
+        }
+    }
+
+    
+
+    /*    *****    DELETE Facility    *****       */
+    const deleteFacility = (facility) => {
+       
+        const facilityId = facility._id;
+        axios.delete(`http://localhost:5000/api/admin/${facilityId}`)
+            .then(data => {
+                // console.log(data)
+                setChangeAfterDelete(!changeAfterDelete)
+            })
+    }
+    /*    *****    DELETE USER    *****       */
+    const deleteUser = (user) => {
+       
+        const userId = user._id;
+        axios.delete(`http://localhost:5000/api/admin/${userId}/deleteuser`)
+            .then(data => {
+                // console.log(data)
+                setChangeAfterDelete(!changeAfterDelete)
+            })
+    }
+
+
+    
+
+    /*    *****    MAP ALL PRODUCTS    *****       */
+    const allFacilities = facilities.map(facility => {
+
+        return (
+            <>
+                <div className='content-container'>
+                    <img
+                        src={facility.images[0]}
+                        alt=""
+                    />
+                    <h5 className="container-title">{facility.name}</h5>
+
+                    <p className="admin-delete-btn" onClick={() => deleteFacility(facility)}>X</p>
+                </div>
+            </>
+        )
+    })
+
+
+    /*    *****    MAP ALL USERS    *****       */
+    const allUsers = users.map(user => {
+
+        return (
+            <>
+                <div className='content-container'>
+                    <img
+                        src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'
+                        alt=""
+                    />
+                    <h5 className="container-title">Name: {user.name}, Email: {user.email}, </h5> 
+
+                    <p style={{ marginTop: '12px', float: 'left', marginRight:'10px', fontSize:'18px' }}>Admin: </p>
+                    
+                    <BootstrapSwitchButton
+                        checked={user.isAdmin}
+                        onlabel='Yes'
+                        offlabel='NO'
+                        onChange={(e)=>changeUserPermission(e,user)}
+                    />
+
+                    <p className="admin-delete-btn" onClick={() => deleteUser(user)}>X</p>
+                </div>
+            </>
+        )
+    })
+
+
+    /*    *****    RENDER ADMIN PAGE    *****       */
+
+    if (!props.auth.isLoggedIn) {
+        return (
+            <>
+                <div class="alert alert-danger" role="alert">
+                    <strong>Oh !!!</strong> <Link to="/login" class="alert-link">You have to login first</Link> and try submitting again.
+                </div>
+            </>
+        )
+    }
+     else if (!props.auth.currentUser.isAdmin) {
+        return (
+            <>
+                <div class="alert alert-warning" role="alert">
+                    <strong>Oh !!!</strong> <Link to="/" class="alert-link">You dont have permission</Link>  to access this page.
+                </div>
+            </>
+        )
+    } 
+    else {
+        return (
+            <div className="dashboard-container ">
+
+            
+                <h1>Facilities</h1>
+                <div className='admin-container'>
+                    {allFacilities}
+                </div> <br />
+
+                <h1>Users</h1>
+                <div className='admin-container'>
+                    {allUsers}
+                </div>
+
+                <h1>Confirm addition of facility</h1>
+                <div className='admin-container'>
+                  
+                </div>
+                
+            </div>
+        )
+    }
+
+}
