25a2e00b198a73951b8dac16b3c8f88ecfc550fb
talamajed <tala.majed.g@gmail.com>
2021-02-14 11:07:59 +0300

    back-end ratings

---
+++ Back-End/models/rating.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+const ratingSchema = new mongoose.Schema({
+    comment : {
+        type :String , 
+        required :true ,
+    },
+    stars : {
+        type :Number , 
+        required :true
+    } , 
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+} , {timestamps :true})
+
+
+const Rating = mongoose.model('rating' , ratingSchema)
+module.exports = Rating;
---
+++ Back-End/routes/rating.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const Facility = require('../models/facility')
+const Rating = require('../models/rating')
+
+
+
+// show one facility ratings 
+router.get('/:facilityId/ratings', (req, res) => {
+    let facilityId = req.params.facilityId
+
+       Facility.findOne({_id: facilityId}).populate('rating')
+      .then(rating=>{
+
+       res.json({ msg: "rating Info", rating});
+  
+      })
+    
+  }) 
+
+  //add rating
+  router.post("/:facilityId/new-rating", (req, res) => {
+   
+    const { comment,stars ,userId} = req.body;
+    let facilityId = req.params.facilityId
+    Rating.create({ comment : comment , stars : stars ,userId: user}, (err, newRating) => {
+  
+      Facility.updateOne({ _id: facilityId }, { $push: { rating: `${newRating._id}` } })
+        .then(() => {
+  
+          res.json({ msg: "You added a new rating", newRating})
+        })
+    })
+  });
+  //edit
+
+  //delete
+  router.delete('/:ratingId/:facilityId', (req, res) => {
+    let ratingId = req.params.ratingId
+    let facilityId = req.params.facilityId
+    Rating.findByIdAndDelete(appointmentId)
+      .then(() => {
+        Facility.findByIdAndUpdate(facilityId, { $pull: { rating: ratingId } })
+          .then(() => {
+            res.json({ msg: "Rating is Deleted!" })
+          })
+      })
+  })
+
+  module.exports = router
