157f9de6099c70454f2426af7bf0be400314bf81
ASIM SAMI M ALLUHAYBI <eng.asimsami@gmail.com>
2021-02-14 14:21:45 +0300

    update

--- Back-End/server.js
+++ Back-End/server.js
@@ -16,7 +16,8 @@ mongoose.connect(
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+const path = require('path')
+app.use(express.static(path.join(__dirname , "build")));
 
 //routes
 app.use("/api/user", require("./routes/user"));
@@ -24,8 +25,7 @@ app.use("/api/facility", require("./routes/facility"));
 app.use("/api/appointment", require("./routes/appointment"));
 app.use("/api/admin", require("./routes/admin"));
 
-const path = require('path')
-app.use(express.static(path.join(__dirname , "build")));
+
 
 app.get("*" , (req,res ) =>{
 
