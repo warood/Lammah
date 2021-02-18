6a01d5cb560dc36c5b887fc1f481f6e9984dc9c1
ASIM SAMI M ALLUHAYBI <eng.asimsami@gmail.com>
2021-02-18 06:29:37 +0300

    update

---
+++ Front-End/lammah/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import API_URL from '../apiConfig.js'
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import axios from "axios";
+// import { Link, useHistory } from 'react-router-dom'
+
+
+export default function Login(props) {
+
+  
+  return (
+    <>
+      
+
+     
+
+    </>
+
+  )
+}
